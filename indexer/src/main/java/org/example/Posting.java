422267619c4c4af57172118f6f8e2fdf22600569
matteorazzai1 <118298555+matteorazzai1@users.noreply.github.com>
2023-10-15 16:06:07 +0200

    prova merging of more files
    Each files with an intermediate index

---
+++ indexer/src/main/java/org/example/Posting.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Posting {
+
+        private int docId;
+
+        private int frequency;
+
+        public Posting(int docId,int frequency){
+                this.docId=docId;
+                this.frequency=frequency;
+        }
+
+        public int getDocId() {
+                return docId;
+        }
+
+        public int getFrequency() {
+                return frequency;
+        }
+
+        public void setDocId(int docId) {
+                this.docId = docId;
+        }
+
+        public void setFrequency(int frequency) {
+                this.frequency = frequency;
+        }
+
+        @Override
+        public String toString() {
+                return super.toString();
+        }
+}
