da97283afe3765e95951d936876d1d74be928165
dominicofthebears <domenicodorsi01@gmail.com>
2023-10-25 12:19:11 +0200

    added SPIMI

---
+++ indexer/src/main/java/it/unipi/mircv/InvertedIndex.java
@@ -0,0 +1,20 @@
+package it.unipi.mircv;
+
+import java.util.HashMap;
+
+public class InvertedIndex {
+
+    private HashMap<String, PostingList> PostingLists;
+
+    public InvertedIndex(){
+        PostingLists = new HashMap<>();
+    }
+
+    public HashMap<String, PostingList> getPostingLists() {
+        return PostingLists;
+    }
+
+    public void setPostingLists(HashMap<String, PostingList> postingLists) {
+        this.PostingLists = postingLists;
+    }
+}
